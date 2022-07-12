bd20535356ba1393e221c697e3de29bab8251ddd
ThePhenomenalP <12345rezatabrizi@gmail.com>
2022-07-12 06:27:05 +0430

    Edit Template Part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SolarDatePipe } from './Pipes/solar-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolarDatePipe
   ],
   imports: [
     BrowserModule
